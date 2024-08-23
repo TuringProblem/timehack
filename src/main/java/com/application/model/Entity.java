94d9373f23c96cf5873c987da6c989ea3ee6d88c
TuringProblem <andrew.deca.wellington@gmail.com>
2024-08-23 06:43:39 -0400

    feat: added more to pull

--- src/main/java/com/application/model/Entity.java
+++ src/main/java/com/application/model/Entity.java
@@ -1,4 +1,33 @@
+
 public abstract class Entity {
-  
-  protected 
+
+  private final UUID id;
+  private String ownerUsername;
+
+  pbulic Entity(String ownerUsername) {
+    this.ownerUsername = ownerUsername;
+  }
+
+  public UUID getID() {
+    return id;
+  }
+
+  public String getOwnerUsername() {
+    return ownerUsername;
+  }
+
+  public void setID(UUID id) {
+    this.id = id;
+  }
+
+  public void setOwnerUsername(String ownerUsername) {
+    this.ownerUsername = ownerUsername;
+  }
+
+  public abstract void validate();
+
+  @Override
+  public String toString() {
+  }
+
 }
