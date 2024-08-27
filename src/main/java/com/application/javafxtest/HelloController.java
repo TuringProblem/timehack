38632efdfe0e3af6d43b77ccc3c4d7bbb39962df
Taziz <122590935+TuringProblem@users.noreply.github.com>
2024-08-26 21:52:55 -0400

    feat: started working on the Sign up section, and need to make sure that I study more on the structure of how a fxml and just JavaFX project is suppose to go; I know that the controller class is the brains (backend)but need to figure out remaining

--- src/main/java/com/application/javafxtest/HelloController.java
+++ src/main/java/com/application/javafxtest/HelloController.java
@@ -1,14 +1,43 @@
 package com.application.javafxtest;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML public Hyperlink signUpLink;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void initialize() {
+        signUpLink.setOnAction(event -> {
+            try {
+                openSignUpScene();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    @FXML
+    private void openSignUpScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/application/javafxtext/views/signup.fxml"));
+        AnchorPane signUpPane = loader.load();
+
+        Stage stage = (Stage) signUpLink.getScene().getWindow();
+        Scene scene = new Scene(signUpPane);
+        stage.setScene(scene);
     }
 }
---
+++ src/main/java/com/application/javafxtest/SignUpController.java
@@ -0,0 +1,15 @@
+package com.application.javafxtest;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SignUpController {
+    @FXML private Button backToLoginButton;
+
+    @FXML
+    private void initializer() {
+        backToLoginButton.setOnAction(actionEvent ->  goBackToLogin());
+    }
+    @FXML
+    private void goBackToLogin() {}
+}
